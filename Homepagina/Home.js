1e4d3f81c959387536eb17238c0ad2b50ca055c0
Lorenzo Breedveld <2105144@talnet.nl>
2019-11-04 14:07:47 +0100

    einde dag

---
+++ Homepagina/Home.js
@@ -0,0 +1,11 @@
+function verander1(){
+document.getElementById("Foto1").style.display= "none";
+document.getElementById("foto2").style.display= "block";
+
+}
+
+function verander2(){
+    document.getElementById("Foto1").style.display= "block";
+    document.getElementById("foto2").style.display= "none";
+    
+    }
